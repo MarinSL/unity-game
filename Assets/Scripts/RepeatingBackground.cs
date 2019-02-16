0d7246beb98b0a69b6a41815083d81621c01e6ea
Oskar Bergman <kemioskar@gmail.com>
2019-02-16 14:45:42 +0100

    Revert "Merge branch 'Master-test' into Oskar_Test-Branch"
    This reverts commit 1eaf07366004b910a179338c6cf2658e4f712a87, reversing
    changes made to a9806ccc993c6a345e32c038bd8e7de0fd4cc1e3.

---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D backCollider;
+    private float backgroundLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backCollider = GetComponent<BoxCollider2D>();
+        backgroundLength = backCollider.size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > backgroundLength/2)
+        {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 backgroundOffset = new Vector2(0,- backgroundLength * 2f);
+        transform.position = (Vector2)transform.position + backgroundOffset;
+        transform.position =transform.position + new Vector3(0,0,2);
+    }
+
+}
---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+
+    public float speed = 10f;
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       
+    }
+
+    void Update()
+    {
+        //set active
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!col.gameObject.CompareTag("bullet"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
